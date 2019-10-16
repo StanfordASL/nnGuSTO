7db23c6718f8546c090c09791949280d53f8b3a5
Somrita Banerjee <sb892@cornell.edu>
2019-10-16 15:55:03 -0700

    Preparing to gather poly data

--- astrobee_se3_script.jl
+++ astrobee_se3_script.jl
@@ -187,3 +187,27 @@ function plot_circle(position_2d, radius; color=:blue, fig=-1, lab="")
         return fig
     end
 end
+
+function plot_square(center_2d, width_2d; color=:blue, fig=-1, lab="")
+    # adapted from https://discourse.julialang.org/t/plot-a-circle-with-a-given-radius-with-plots-jl/23295
+    function squareShape(h, k, wh, wk)
+        return Shape(h .+ [-wh/2, wh/2, wh/2, -wh/2], k .+ [-wk/2, -wk/2, wk/2, wk/2])
+        # dh = LinRange(0, wh/2, 500)
+        # dk = LinRange(0, wk/2, 500)
+        # h .+ dh, k .+ dk
+    end
+
+    if fig == -1 # undefined, plot new
+        fig = plot(squareShape(center_2d[1],center_2d[2],width_2d[1],width_2d[2]), 
+                seriestype = [:shape,], lw = 0.5,
+                c = color, linecolor = :black,
+                legend = false, fillalpha = 0.5, aspect_ratio = 1, label = lab)
+    return fig
+    else
+        plot!(fig, squareShape(center_2d[1],center_2d[2],width_2d[1],width_2d[2]), 
+                    seriestype = [:shape,], lw = 0.5,
+                    c = color, linecolor = :black,
+                    legend = false, fillalpha = 0.5, aspect_ratio = 1, label = lab)
+        return fig
+    end
+end
